eb8906013818fc0a1ec4205540e9621440b799ec
Nicorehov <nicorehov@gmail.com>
2025-06-06 23:53:13 +0200

    prometheus request counter added

--- bot/main.py
+++ bot/main.py
@@ -3,12 +3,17 @@ import asyncio
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 from quote import router
+from prometheus_client import Counter, start_http_server
+
 
 load_dotenv()    
 
 async def main():
     BOT_TOKEN = os.getenv("BOT_TOKEN")
     bot = Bot(token=BOT_TOKEN)
+
+    start_http_server(8000)
+
     dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot, skip_updates=True)
--- bot/quote.py
+++ bot/quote.py
@@ -1,10 +1,15 @@
 from aiogram import Router, types
 from aiogram.filters import Command
 from quote_service import get_random_quote
+from prometheus_client import Counter
 
 router = Router()
 
+messages_counter = Counter("telegram_quote_requests_total", 
+                                "Total number of /quote command calls")
+
 @router.message(Command("quote"))
 async def cmd_quote(message: types.Message):
+    messages_counter.inc()
     quote = get_random_quote()
     await message.answer(quote)
